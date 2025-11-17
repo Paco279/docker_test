fd60e06b3d8c98ae5fd4072984019432e5092f4a
Paco B. CAZ <pacoberort@gmail.com>
2025-11-17 19:58:38 +0100

    test(docker_test): probando html en fastapi con jinja2

--- src/docker_test/app.py
+++ src/docker_test/app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "hello world!"}
+# Carpeta de templates
+templates = Jinja2Templates(directory="templates")
+
+# Carpeta de archivos estáticos (CSS, imágenes, etc.)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "¡Hola, mundo!"})
